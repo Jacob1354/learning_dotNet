5c6b7b85b43351e3375863a9fbe9cf76fe3e373e
Salois, Jacob <salois.jacob@courrier.uqam.ca>
2025-09-13 22:28:57 -0400

    Feat: App prints out a file

--- Program.cs
+++ Program.cs
@@ -1,1 +1,24 @@
-﻿Console.WriteLine("Hello, World!");
+﻿namespace System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        var lines = ReadFrom("sampleQuotes.txt");
+        foreach (string line in lines) {
+            Console.WriteLine(line);
+        }
+        
+    }
+    
+    static IEnumerable<string> ReadFrom(string file)
+    {
+        string? line;
+        using (var reader = File.OpenText(file))
+        {
+            while ((line = reader.ReadLine()) != null)
+            {
+                yield return line;
+            }
+        }
+    }
+}
